fc1cd12e908d5a48d61342d9a794a56525af8b0b
bohushenko Alex <bogushenkoas@ukr.net>
2025-12-05 23:51:30 +0100

    fffff

--- main.cpp
+++ main.cpp
@@ -5,47 +5,10 @@
 
 using namespace std;
 
-mutex mtx;
-mutex mtx2;
-string command;
-
-void road(string train_name, int travel_time)
-{
-    mtx.lock();
-    cout << "train " << train_name << " on the way" << endl;
-    mtx.unlock();
-    this_thread::sleep_for(chrono::seconds(travel_time));
-    mtx.lock();
-    cout << "train " << train_name << " arrived at the station" << endl;
-    mtx.unlock();
-
-    mtx2.lock();
-    
-    while(command != "depart")
-    {
-        cout << "enter command depart for " << train_name << endl;
-        cin >> command;
-        if (command == "depart")
-        {
-            command = "";
-            break;
-        }
-    }
-    mtx2.unlock();
-
-    cout << "train " << train_name << " departing from the station" << endl;
-
-}
 
 int main()
 {
-    thread a (road, "A", 7); 
-    thread b (road, "B", 14); 
-    thread c (road, "C", 21); 
-
-    a.join();
-    b.join();
-    c.join();
+    
 
     return 0;
 
