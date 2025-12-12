de7ef7dcff4f8b7e002ed99ed83da6fa724c395a
bohushenko Alex <bogushenkoas@ukr.net>
2025-12-12 20:45:24 +0100

    mnz

--- main.cpp
+++ main.cpp
@@ -10,11 +10,7 @@ using namespace std;
 int main()
 {
     
-<<<<<<< HEAD
-cout << "hello Ruslan" << endl;
-=======
 cout << "hello Denis" << endl;
->>>>>>> 6da3b630f4e3e61de199cc8522bd3d4b970a183a
 
 return 0;
 
